53c95301ca06b51627fab972b45423aa305d853b
Aditya Suryadi <adityasuryaditya@gmail.com>
2025-01-30 08:31:44 +0700

    feat:add client grpc

--- client/main.go
+++ client/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"time"
 
+	pb "github.com/adityasuryadi/belajar-grpc/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient(
+		"localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewGreaterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	r, err := client.SayHello(ctx, &pb.HelloRequest{Name: "world"})
+	if err != nil {
+		log.Printf("an error occured: %v", err)
+		return
+	}
+	log.Printf("Message from server: %s", r.Message)
 }
--- server/main.go
+++ server/main.go
@@ -10,20 +10,27 @@ import (
 )
 
 type server struct {
+	// mengimplementasikan server
 	pb.UnimplementedGreaterServer
 }
 
-func (s *server) SayHelo(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+// method say hello dari protobuff
+// implemetasi dari server
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	// req.Getname() hasil dari generate protobuff
 	log.Println("Received: ", req.GetName())
 	return &pb.HelloReply{Message: "Hello " + req.GetName()}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	// buat server grpc
 	s := grpc.NewServer()
+
+	// menjalankan server grpc menggunakan struct server di atas
 	pb.RegisterGreaterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
